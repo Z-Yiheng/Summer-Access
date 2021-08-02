c3e11261eea0fa4c0f4e0075ce83a88311aa35c6
Z-Yiheng <1191453751@qq.com>
2021-08-02 17:29:37 +0800

    部分页面+集成Markdown 插件

--- js/index.js
+++ js/index.js
@@ -147,3 +147,42 @@ function hideWindow_Wechat() {
     $("#window_Wechat").hide();
     $("#shadow").hide();
 }
+
+// 瀑布流
+function waterFall() {
+    var columns = 3;
+    var itemWidth = 300;
+    var arr = []
+    $(".content_body .item").each(function(i) {
+        var height = $(this).height();
+        if (i < columns) {
+            $(this).css({
+                top: 0,
+                left: (itemWidth) * i + 50 * i,
+            });
+            arr.push(height);
+        } else {
+            var minHeight = arr[0];
+            var index = 0;
+            for (var j = 0; j < arr.length; j++) {
+                if (minHeight > arr[j]) {
+                    minHeight = arr[j];
+                    index = j;
+                }
+            }
+            $(this).css({
+                top: arr[index] + 30,
+                left: $(".content_body .item").eq(index).css("left")
+            });
+            arr[index] = arr[index] + height + 30;
+        }
+    })
+}
+
+window.onresize = function() {
+    waterFall();
+}
+
+window.onload = function() {
+    waterFall();
+}
