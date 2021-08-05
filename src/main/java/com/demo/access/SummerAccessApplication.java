edbf096f003f036b2b2dafbfc772c2411d74e8b3
Z-Yiheng <1191453751@qq.com>
2021-08-05 11:07:05 +0800

    创建idea“

---
+++ src/main/java/com/demo/access/SummerAccessApplication.java
@@ -0,0 +1,13 @@
+package com.demo.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummerAccessApplication.class, args);
+    }
+
+}
